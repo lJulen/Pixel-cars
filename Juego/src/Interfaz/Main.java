374653049e06f2e17282bbdf19e835224cca9567
Julen <julengarro@opendeusto.es>
2016-01-15 16:56:12 +0100

    Version final

--- Juego/src/Interfaz/Main.java
+++ Juego/src/Interfaz/Main.java
@@ -4,7 +4,7 @@ import javax.swing.SwingUtilities;
 import Interfaz.VentanaJuegoCoche.MiRunnable;
 import Interfaz.VentanaJuegoMoto.MiRunnableMoto;
 public class Main {
-
+//
 	public static void main(String[] args) {
 	
 		VentanaPrincipal Menu=new VentanaPrincipal();
